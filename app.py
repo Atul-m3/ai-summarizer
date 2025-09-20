8e811f0d26627f44e79e2706e71d3d1b6cbaff1e
Atul Kumar Singh <singh.atulk9@gmail.com>
2025-09-20 21:28:18 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 st.set_page_config(page_title="AI Summarizer", layout="centered")
 st.title("AI Summarizer & Q&A")
